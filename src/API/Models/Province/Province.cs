084599b229a06b987138c585759e50a8444ce10b
尹庆文 <yinqingwen@gmail.com>
2016-03-03 18:04:58 +0800

    20160303

---
+++ src/API/Models/Province/Province.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.province
+{
+    /// <summary>
+    /// 省份数据库
+    /// </summary>
+    public class Province
+    {
+        /// <summary>
+        /// 省份ID
+        /// </summary>
+        public long ID { get; set; }
+
+        [Required]
+        [MaxLength(6)]
+        public string ProvinceCode { get; set; }
+
+        public string ProvinceName { get; set; }
+    }
+}
